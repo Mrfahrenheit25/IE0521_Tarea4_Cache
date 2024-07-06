32cd7d83ced82c7be3436a3cb13847c69c15955a
Aurelio Cordoba V <acvalerio25@gmail.com>
2024-07-06 14:16:40 -0600

    Correción de los errores de los scripts

--- parte_I/scripts/todo.bat
+++ parte_I/scripts/todo.bat
@@ -1,21 +1,20 @@
 @echo off
 rem Cambia el directorio a la ubicación de tu script de Python, si es necesario
-cd /d C:\Users\acval\OneDrive\Escritorio\UCR\Sem VII\EstrucComp II\baseline_cache_I2024\IE0521_Tarea4_Cache\parte_I\scripts
-
-call script.bat
+cd /d C:\Users\acval\OneDrive\Escritorio\UCR\Sem VII\EstrucComp II\baseline_cache_I2024\IE0521_Tarea4_Cache\parte_I
+call scripts\script.bat
 rem Llamar a script2.bat
-call script2.bat
-call script3.bat
-call script4.bat
-call script5.bat
-call scripta.bat
-call scripta2.bat
-call scripta4.bat
-call scripta8.bat
-call scripta16.bat
-call scriptb16.bat
-call scriptb32.bat
-call scriptb64.bat
-call scriptb128.bat
-call scriptr.bat
-call scriptprueba.bat
+call scripts\script2.bat
+call scripts\script3.bat
+call scripts\script4.bat
+call scripts\script5.bat
+call scripts\scripta.bat
+call scripts\scripta2.bat
+call scripts\scripta4.bat
+call scripts\scripta8.bat
+call scripts\scripta16.bat
+call scripts\scriptb16.bat
+call scripts\scriptb32.bat
+call scripts\scriptb64.bat
+call scripts\scriptb128.bat
+call scripts\scriptr.bat
+call scripts\scriptprueba.bat
